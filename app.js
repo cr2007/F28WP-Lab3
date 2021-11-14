b16e0ccb3edbebe8f587edcbd8642910bfa45de9
Chandrashekhar R <cr2007@hw.ac.uk>
2021-11-14 22:04:50 +0400

    Creates `app.js` file
    Loads the express module, creates an app, HTTP
    server, and makes the server listen on port 3000

---
+++ app.js
@@ -0,0 +1,10 @@
+const express = require('express');
+
+// Creates the app
+const app = express();
+
+//Make the app listen on port
+const port = process.argv[2] || process.env.PORT || 3000;
+const server = app.listen(port, () => {
+    console.log(`Cart app listening at http://localhost:${port}`);
+});
