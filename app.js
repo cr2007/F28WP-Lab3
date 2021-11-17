50f96eb818a44d125beb7dfad1f29e87079f1164
Chandrashekhar R <cr2007@hw.ac.uk>
2021-11-17 09:26:20 +0400

    Adds `app.get()` line

--- app.js
+++ app.js
@@ -3,6 +3,12 @@ const express = require('express');
 // Creates the app
 const app = express();
 
+// Sends an HTTP Response when receiving HTTP GET /
+
+app.get('/', (req, res) => {
+    res.send("Hello, your server is up and running");
+});
+
 //Make the app listen on port
 const port = process.argv[2] || process.env.PORT || 3000;
 const server = app.listen(port, () => {
